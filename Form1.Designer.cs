f42aa87c84bbfc23b13374d9fc0a7ae8fd9fc124
Jarvis <1763241030@qq.com>
2021-01-03 22:22:04 +0800

    可统计单文本参数 21010301

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.打开 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // 打开
+            // 
+            this.打开.Location = new System.Drawing.Point(316, 25);
+            this.打开.Name = "打开";
+            this.打开.Size = new System.Drawing.Size(75, 21);
+            this.打开.TabIndex = 0;
+            this.打开.Text = "打开";
+            this.打开.UseVisualStyleBackColor = true;
+            this.打开.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(413, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(375, 426);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "统计";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(28, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "输入：";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 79);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 129);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(14, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "例：交易成功\r\n    交易失败\r\n    黑名单";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(4, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "文件选择：";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "清空";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.打开);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button 打开;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        String fName; // 存储打开文件路径
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //
+        //                       _oo0oo_
+        //                      o8888888o
+        //                      88" . "88
+        //                      (| -_- |)
+        //                      0\  =  /0
+        //                    ___/`---'\___
+        //                  .' \\|     |// '.
+        //                 / \\|||  :  |||// \
+        //                / _||||| -:- |||||- \
+        //               |   | \\\  -  /// |   |
+        //               | \_|  ''\---/''  |_/ |
+        //               \  .-\__  '-'  ___/-. /
+        //             ___'. .'  /--.--\  `. .'___
+        //          ."" '<  `.___\_<|>_/___.' >' "".
+        //         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+        //         \  \ `_.   \_ __\ /__ _/   .-` /  /
+        //     =====`-.____`.___ \_____/___.-`___.-'=====
+        //                       `=---='
+        //
+        //
+        //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //
+        //               佛祖保佑         永无BUG
+        //
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 转UTF-8
+        public static string get_uft8(string unicodeString)
+        {
+            UTF8Encoding utf8 = new UTF8Encoding();
+            Byte[] encodedBytes = utf8.GetBytes(unicodeString);
+            String decodedString = utf8.GetString(encodedBytes);
+            return decodedString;
+        }
+
+        // 循环统计文本字符串中某一字符出现的次数
+        public int CalauteCharShowCount_For(char c, string text)
+        {
+            int count = 0; // 定义一个计数器
+
+            // 循环统计
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == c)
+                    count++;
+            }
+            return count;
+        }
+
+        // 循环统计文本字符串中某一字符串出现的次数
+        // Inputstr：需输入查询字符串
+        // text:正则表达式
+        public int CalauteStringShowCount_For(string Inputstr, string text)
+        {
+            int count = 0; // 定义一个计数器
+            MatchCollection mc = Regex.Matches(Inputstr, text);
+
+            // 循环统计
+            foreach (Match m in mc)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog(); // 选择文件
+            //FolderBrowserDialog fbd = new FolderBrowserDialog(); // 选择文件夹
+            //fbd.ShowDialog(); // 选择文件夹
+            //textBox1.Text = fbd.SelectedPath;
+
+            openDialog.ShowDialog(); // 选择文件
+            textBox1.Text = openDialog.FileName;
+
+            fName = openDialog.FileName;
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // 统计按钮监听事件
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            String InputString; // 存储输入字符串
+            int count = 0;
+            String str1;
+
+
+            StreamReader Str = new StreamReader(fName, Encoding.Default); // 创建一个StreamReader 的实例来读取文件
+            InputString = textBox2.Text; // 获取输入字符串
+            str1 = Str.ReadToEnd(); // 读取文件中数据
+
+            String[] sArray = InputString.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+            foreach (string i in sArray)
+            {
+                count = CalauteStringShowCount_For(str1, @i.ToString()); // 计算字符串出现次数
+                richTextBox1.AppendText(i.ToString() + ":" + count.ToString() + "\r\n"); // 显示数据
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
